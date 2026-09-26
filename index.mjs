50efbe732f39be8294cd38a8f7e256eb5b13f9ef
Mohan k <mohanarul2003@gmail.com>
2026-09-26 12:51:15 +0530

    quiery

--- index.mjs
+++ index.mjs
@@ -4,30 +4,41 @@ const app = express();
 
 const PORT = 3000;
 
+//project queiry
 app.get("/", (req, res) => {
   res.send({ msg: "root" });
 });
-const product = [
+const project = [
   { id: 1, item: "amazon-clone " },
   { id: 2, item: "instagram-clone " },
-  { id: 3, item: "amazon-clone " },
-  { id: 4, item: "amazon-clone " },
-  { id: 5, item: "amazon-clone " },
-  { id: 6, item: "amazon-clone " },
+  { id: 3, item: "AI-crm module " },
+  { id: 4, item: "todo list " },
+  { id: 5, item: "calculator " },
+  { id: 6, item: "stone-paper-scissor game " },
 ];
 
-app.get("/api/product", (req, res) => {
-  res.send({ product });
+app.get("/api/project", (req, res) => {
+  const {
+    query: { filter, value },
+  } = req;
+  console.log(req.query);
+  console.log(filter, value);
+  if (filter && value) {
+    return res.send(
+      project.filter((item) => item[filter].toLowerCase().includes(value)),
+    );
+  }
+  res.send({ project });
 });
 
-app.get("/api/product/:id", (req, res) => {
+app.get("/api/project/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   console.log(id);
   if (isNaN(id)) {
     return res.status(400).send({ msg: "item not found" });
   }
-  const item = product.find((product) => product.id === id);
+  const item = project.find((project) => project.id === id);
   if (item) {
     return res.send(item);
   }
@@ -42,7 +53,19 @@ const users = [
   { id: 4, user_name: "revathy" },
   { id: 5, user_name: "muthu" },
 ];
+//query parameter
 app.get("/api/users", (req, res) => {
+  const {
+    query: { filter, value },
+  } = req;
+  // console.log(req.query);
+  // console.log(filter, value);
+  if (filter && value) {
+    return res.send(
+      users.filter((user) => user[filter].toLowerCase().includes(value)),
+    );
+  }
+
   res.send({ users });
 });
 
@@ -53,10 +76,10 @@ app.get("/api/users/:id", (req, res) => {
     return res.status(400).send({ msg: "bad Request, invalid Id" });
   }
 
-  const user = users.find((user) => user.id === id);
-  if (user) {
-    return res.send(user);
-  }
+  // const user = users.find((user) => user.id === id);
+  // if (user) {
+  //   return res.send(user);
+  // }
   return res.status(404).send({ msg: "user not found" });
   //   console.log(user)
 });
@@ -65,5 +88,5 @@ app.listen(PORT, () => {
   console.log(`app is running on ${PORT} `);
 });
 
-
-// localhost:3000/users?
+// http://localhost:3000/api/users?filter=user_name&value=arul
+// http://localhost:3000/api/project?filter=item&value=ama
